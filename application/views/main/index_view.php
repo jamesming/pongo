90fc5967550a177f01c6788ddd73b40d672f7354
unknown <James Ming@JamesMing-THINK.(none)>
2012-02-16 07:44:57 -0800

    added

--- application/views/main/index_view.php
+++ application/views/main/index_view.php
@@ -127,13 +127,12 @@
 					</style>
 <link href="http://vjs.zencdn.net/c/video-js.css" rel="stylesheet">
 <script src="http://vjs.zencdn.net/c/video.js"></script>
-					<div  id='video_container'>
-							<video id="my_video_1" class="video-js vjs-default-skin" controls
-							  preload="auto" width="640" height="400" poster="my_video_poster.png"
-							  data-setup="{}">
-							  <source src="<?php  echo base_url()   ?>images/pongo.mp4" type='video/mp4'>
-							</video>		
-					</div>
+<video id="my_video_1" class="video-js vjs-default-skin" controls
+  preload="auto" width="640" height="264" poster="my_video_poster.png"
+  data-setup="{}">
+  <source src="my_video.mp4" type='video/mp4'>
+  <source src="my_video.webm" type='video/webm'>
+</video>
 
 
 	</div>
